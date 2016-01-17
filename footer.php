18426cb6d2872d75977066f49001fec1bed43e87
Jeff Thompson <mail@jeffreythompson.org>
2016-01-17 11:02:41 -0500

    Updated Github repo link

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
 			
 			<ul>
 				<li>A project by <a href="http://www.jeffreythompson.org">Jeff Thompson</a></li>
-				<li><a href="#">Code/full data</a> for this project</li>
+				<li><a href="https://github.com/jeffThompson/TCPDUMP">Code/full data</a> for this project</li>
 				<li>CC <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">BY-NC-SA</a></li>
 				<li>2014–15</li>
 			</ul>
